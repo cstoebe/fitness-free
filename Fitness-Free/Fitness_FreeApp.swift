198db3fe506a6cc3b53e0489fda0954266ba98b3
Conner Stoebe <cbstoebe@gmail.com>
2023-08-10 14:23:16 -0700

    Initial Commit

---
+++ Fitness-Free/Fitness_FreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_FreeApp.swift
+//  Fitness-Free
+//
+//  Created by Conner Stoebe on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_FreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
